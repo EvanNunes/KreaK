25d6686b5157a2f15f32cfb8b718ba6b1b57cf7c
EvanNunes <evan.nunes2005@gmail.com>
2026-03-27 16:58:17 +0100

    feat : Create the first route (/api/products)

--- back/src/Controller/ProductController.php
+++ back/src/Controller/ProductController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+
+    public function __construct(
+        private ProductRepository $productRepository
+
+    ) {}
+
+    #[Route('/api/products', name: 'api_products', methods: ['GET'])]
     public function index(): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $products = $this->productRepository->findAll();
+
+        $data = array_map(function($product) {
+            return [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'description' => $product->getDescription(),
+                'price' => $product->getPrice(),
+                'category' => $product->getCategory(),
+                'imageUrl' => $product->getImageUrl(),
+            ];
+        }, $products);
+
+        return new JsonResponse($data);
     }
 }
